e48f470b4ccfa9419cea71ebe44632a74567d9f6
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-21 19:22:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,4 +6,9 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  
 }
