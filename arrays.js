bc5dee4b4bdc197dcedff7f11a4240bd8c80ff1c
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-21 19:27:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,4 +22,6 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  
+}
